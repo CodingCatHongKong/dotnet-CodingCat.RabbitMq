dc2f73f4a77445ca2cfa28522e3c8dcd43963577
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-16 19:46:21 +0800

    added new library project for .Net 4.6.1+ and .Net Standard 2+ to enhance the pub/sub experience

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Abstracts/BaseBasicPublisher.cs
@@ -0,0 +1,39 @@
+﻿using CodingCat.RabbitMq.Interfaces;
+using RabbitMQ.Client;
+using System;
+
+namespace CodingCat.RabbitMq.PubSub.Abstracts
+{
+    public abstract class BaseBasicPublisher
+    {
+        public IExchangeProperty ExchangeProperty { get; set; }
+        public IQueue UsingQueue { get; set; }
+
+        public string RoutingKey { get; set; }
+        public bool IsMandatory { get; set; } = false;
+
+        protected void Publish(byte[] body, IBasicProperties properties)
+        {
+            if (this.UsingQueue.Channel == null)
+                throw new InvalidOperationException(
+                    "the queue is not yet declared"
+                );
+
+            var exchangeName = this.ExchangeProperty?.Name ?? "";
+            properties = this.GetOrCreateProperties(properties);
+
+            this.UsingQueue.Channel
+                .BasicPublish(
+                    exchange: exchangeName,
+                    routingKey: this.RoutingKey,
+                    mandatory: this.IsMandatory,
+                    basicProperties: properties,
+                    body: body
+                );
+        }
+
+        protected IBasicProperties GetOrCreateProperties(
+            IBasicProperties properties
+        ) => properties ?? this.UsingQueue.Channel.CreateBasicProperties();
+    }
+}
---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Impls/BasicPublisher.cs
@@ -0,0 +1,79 @@
+﻿using CodingCat.RabbitMq.PubSub.Abstracts;
+using CodingCat.Serializers.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Threading;
+
+namespace CodingCat.RabbitMq.PubSub.Impls
+{
+    public class BasicPublisher<TInput> : BaseBasicPublisher
+    {
+        public ISerializer<TInput> InputSerializer { get; set; }
+
+        public void Send(TInput input)
+        {
+            this.Send(input, null);
+        }
+
+        public void Send(TInput input, IBasicProperties properties)
+        {
+            var body = this.InputSerializer.ToBytes(input);
+            this.Publish(body, properties);
+        }
+    }
+
+    public class BasicPublisher<TInput, TOutput> : BaseBasicPublisher
+    {
+        public const string REPLY_HEADER = "reply-to";
+
+        public TimeSpan Timeout { get; set; } = TimeSpan.FromMinutes(5);
+        public TOutput DefaultValue { get; set; }
+
+        public ISerializer<TInput> InputSerializer { get; set; }
+        public ISerializer<TOutput> OutputSerializer { get; set; }
+
+        public TOutput Send(TInput input, IBasicProperties properties)
+        {
+            var queueName = this.UsingQueue.Channel.QueueDeclare(
+                queue: string.Empty,
+                durable: false,
+                exclusive: false,
+                autoDelete: true
+            ).QueueName;
+
+            var body = this.InputSerializer.ToBytes(input);
+            properties = this.GetOrCreateProperties(properties);
+            properties.ReplyTo = queueName;
+
+            this.Publish(body, properties);
+
+            return this.Subscribe(queueName);
+        }
+
+        private TOutput Subscribe(string queueName)
+        {
+            var reset = new AutoResetEvent(false);
+            var output = this.DefaultValue;
+
+            var channel = this.UsingQueue.Channel;
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, @event) =>
+            {
+                try
+                {
+                    output = this.OutputSerializer.FromBytes(@event.Body);
+                }
+                catch { }
+
+                reset.Set();
+                channel.BasicCancel(consumer.ConsumerTag);
+            };
+            channel.BasicConsume(queueName, true, consumer);
+
+            reset.WaitOne();
+            return output;
+        }
+    }
+}
