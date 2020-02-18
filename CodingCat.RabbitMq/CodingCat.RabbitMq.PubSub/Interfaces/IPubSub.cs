814c39ef5ab2282c1b1b10d861093207c2d453c3
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-18 21:34:01 +0800

    initialized IPubSub and ITimeoutPubSub

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Interfaces/IPubSub.cs
@@ -0,0 +1,23 @@
+﻿using CodingCat.RabbitMq.Interfaces;
+using CodingCat.Serializers.Interfaces;
+using System;
+
+namespace CodingCat.RabbitMq.PubSub.Interfaces
+{
+    public interface IPubSub : IDisposable
+    {
+        event EventHandler Disposing;
+
+        IQueue UsingQueue { get; }
+    }
+
+    public interface IPubSub<TIntput>
+    {
+        ISerializer<TIntput> InputSerializer { get; }
+    }
+
+    public interface IPubSub<TInput, TOutput> : IPubSub<TInput>
+    {
+        ISerializer<TOutput> OutputSerializer { get; }
+    }
+}
---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Interfaces/ITimeoutPubSub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingCat.RabbitMq.PubSub.Interfaces
+{
+    public interface ITimeoutPubSub
+    {
+        TimeSpan Timeout { get; }
+    }
+}
