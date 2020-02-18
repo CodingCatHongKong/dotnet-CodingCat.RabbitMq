e6ae12e2a346fa9cca6077bd3ddd7e41d101fd81
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-18 22:02:13 +0800

    implemented ISubscriber

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Interfaces/ISubscriber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingCat.RabbitMq.PubSub.Interfaces
+{
+    public interface ISubscriber
+    {
+        event EventHandler MessageCompleted;
+
+        bool IsAutoAck{ get; }
+    }
+
+    public interface ISubscriber<TInput>
+    {
+        TInput DefaultInput { get; }
+    }
+
+    public interface ISubscriber<TInput, TOutput> : ISubscriber<TInput>
+    {
+    }
+}
