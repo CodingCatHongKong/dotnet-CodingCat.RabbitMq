873a2bc317f52431317afcc2a93a6c582266c04e
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-18 21:50:42 +0800

    added IPublisher

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.PubSub/Interfaces/IPublisher.cs
@@ -0,0 +1,26 @@
+﻿using CodingCat.RabbitMq.Interfaces;
+using RabbitMQ.Client;
+using System;
+
+namespace CodingCat.RabbitMq.PubSub.Interfaces
+{
+    public interface IPublisher
+    {
+        IExchangeProperty ExchangeProperty { get; }
+        string RoutingKey { get; }
+        bool IsMandatory { get; }
+    }
+
+    public interface IPublisher<TInput>
+    {
+        void Send(TInput input, IBasicProperties properties = null);
+    }
+
+    public interface IPublisher<TInput, TOutput>
+    {
+        TOutput DefaultOutput { get; }
+        TimeSpan CheckReplyInterval { get; }
+
+        TOutput Process(TInput input, IBasicProperties properties = null);
+    }
+}
