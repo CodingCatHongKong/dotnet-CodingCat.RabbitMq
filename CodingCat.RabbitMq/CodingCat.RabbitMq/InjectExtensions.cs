241255f538946f1f5d60fd2da6d4f6f5a69ec031
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-06 20:52:37 +0800

    added inject extensions for iexchange, iqueue and isubscriberfactory

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq/InjectExtensions.cs
@@ -0,0 +1,31 @@
+﻿using CodingCat.RabbitMq.Abstractions.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodingCat.RabbitMq
+{
+    public static class InjectExtensions
+    {
+        public static IServiceCollection AddExchange<T>(
+            this IServiceCollection services,
+            T exchange
+        ) where T : class, IExchange
+        {
+            return services.AddSingleton(exchange);
+        }
+
+        public static IServiceCollection AddQueue<T>(
+            this IServiceCollection services,
+            T queue
+        ) where T : class, IQueue
+        {
+            return services.AddSingleton(queue);
+        }
+
+        public static IServiceCollection AddFactory<T>(
+            this IServiceCollection services
+        ) where T : class, ISubscriberFactory
+        {
+            return services.AddTransient<ISubscriberFactory, T>();
+        }
+    }
+}
