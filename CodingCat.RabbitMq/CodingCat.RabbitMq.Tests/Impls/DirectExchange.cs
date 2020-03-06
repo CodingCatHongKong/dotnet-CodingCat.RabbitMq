2817557496bc7e3968ab83e1a46769d960717ec2
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-06 22:36:34 +0800

    implemented concrete classes in the test project

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.Tests/Impls/DirectExchange.cs
@@ -0,0 +1,19 @@
+﻿using CodingCat.RabbitMq.Abstractions;
+
+namespace CodingCat.RabbitMq.Tests.Impls
+{
+    public class DirectExchange : BasicExchange
+    {
+        #region Constructor(s)
+
+        public DirectExchange() : base()
+        {
+            this.ExchangeType = ExchangeTypes.Direct;
+            this.IsAutoDelete = false;
+            this.IsDurable = false;
+            this.Name = nameof(DirectExchange);
+        }
+
+        #endregion Constructor(s)
+    }
+}
---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq.Tests/Impls/StringSubscriberFactory.cs
@@ -0,0 +1,24 @@
+﻿using CodingCat.Mq.Abstractions.Interfaces;
+using CodingCat.RabbitMq.Abstractions.Interfaces;
+
+namespace CodingCat.RabbitMq.Tests.Impls
+{
+    public class StringSubscriberFactory : DelegatedSubscriberFactory
+    {
+        #region Constructor(s)
+
+        public StringSubscriberFactory(
+            IQueue queue,
+            IProcessor<string> processor
+        ) : base()
+        {
+            this.DelegatedCreateSubscriber = channel =>
+                new StringSubscriber(channel, queue.Name, processor)
+                {
+                    IsAutoAck = true
+                };
+        }
+
+        #endregion Constructor(s)
+    }
+}
