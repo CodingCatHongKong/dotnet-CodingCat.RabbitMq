c675869c9e8daeddcbb229cf46112e5e0865ff46
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-06 20:52:46 +0800

    added resolve extensions for iexchange and iqueue

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq/ResolveExtensions.cs
@@ -0,0 +1,37 @@
+﻿using CodingCat.RabbitMq.Abstractions.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CodingCat.RabbitMq
+{
+    public static class ResolveExtensions
+    {
+        public static IExchange ResolveExchange<T>(
+            this IServiceProvider provider
+        ) where T : class, IExchange
+        {
+            return provider.GetService<T>();
+        }
+
+        public static IExchange RequireExchange<T>(
+            this IServiceProvider provider
+        ) where T : class, IExchange
+        {
+            return provider.GetRequiredService<T>();
+        }
+
+        public static IQueue ResolveQueue<T>(
+            this IServiceProvider provider
+        ) where T : class, IQueue
+        {
+            return provider.GetService<T>();
+        }
+
+        public static IQueue RequireQueue<T>(
+            this IServiceProvider provider
+        ) where T : class, IQueue
+        {
+            return provider.GetRequiredService<T>();
+        }
+    }
+}
