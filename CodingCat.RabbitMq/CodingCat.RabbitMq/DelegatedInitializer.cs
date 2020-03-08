3c93023792575879b0a5faad0066130c14b6304b
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-08 19:39:26 +0800

    added DelegatedInitializer

---
+++ CodingCat.RabbitMq/CodingCat.RabbitMq/DelegatedInitializer.cs
@@ -0,0 +1,35 @@
+﻿using CodingCat.RabbitMq.Abstractions;
+using CodingCat.RabbitMq.Abstractions.Interfaces;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+
+namespace CodingCat.RabbitMq
+{
+    public class DelegatedInitializer : BaseInitializer
+    {
+        public delegate void ConfigureRabbitMq(IModel channel);
+
+        public ConfigureRabbitMq DelegatedConfigure { get; }
+
+        #region Constructor(s)
+
+        public DelegatedInitializer(
+            IModel channel,
+            IEnumerable<ISubscriberFactory> factories,
+            IEnumerable<IExchange> exchanges,
+            IEnumerable<IQueue> queues,
+            ConfigureRabbitMq delegatedConfigure
+        ) : base(channel, factories, exchanges, queues)
+        {
+            this.DelegatedConfigure = delegatedConfigure;
+        }
+
+        #endregion Constructor(s)
+
+        public override IInitializer Configure(IModel channel)
+        {
+            this.DelegatedConfigure(channel);
+            return this;
+        }
+    }
+}
